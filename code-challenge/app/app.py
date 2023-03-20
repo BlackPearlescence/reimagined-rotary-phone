f89f36b247033824a6c3dc2eefcd743fbd96ab46
Sani Rogers <sanirogers138@gmail.com>
2023-03-20 13:20:32 -0400

    do practice code challenge

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -2,8 +2,10 @@
 
 from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
+from json import dumps
+from sqlalchemy.exc import IntegrityError
 
-from models import db, Restaurant, Pizza, RestaurantPizza
+from models import db, Restaurant, Pizza, RestaurantPizza, PizzaError,RestaurantError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -16,23 +18,78 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return ''
+    return 'Pizza-Restaurant RESTFUL API homepage'
 
 @app.route('/restaurants')
 def restaurants():
-    return ''
+    rest_list = []
+    for restaurant in Restaurant.query.all():
+        # rest = {
+        #     "id": restaurant.id,
+        #     "name": restaurant.name,
+        #     "address": restaurant.address
+        # }
+        rest_list.append(restaurant.to_dict_summary())
 
-@app.route('/restaurants/<int:id>')
+    return make_response(jsonify(rest_list),200)
+
+@app.route('/restaurants/<int:id>', methods = ["GET","DELETE"])
 def restaurant_by_id(id):
-    return ''
+    restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+    if restaurant == None:
+        return make_response(
+            {"error":"Restaurant not found"},
+            404)
+    
+    if request.method == "GET":
+        restaurant_dict = restaurant.to_dict()
+        return make_response(jsonify(restaurant_dict),200)
+    elif request.method == "DELETE":
+        restaurant_pizzas = RestaurantPizza.query.filter(RestaurantPizza.restaurant_id == id).all()
+        # Delete restaurant pizzas
+        for rest_pizza in restaurant_pizzas:
+            db.session.delete(rest_pizza)
+        db.session.commit()
+
+        # Delete the restaurant
+        db.session.delete(restaurant)
+        db.session.commit()
+        return make_response("",200)
+        
 
 @app.route('/pizzas')
 def pizzas():
-    return ''
+    pizza_list = []
+    for pizza in Pizza.query.all():
+        pizza_list.append(pizza.to_dict())
 
-@app.route('/restaurant_pizzas')
-def resaurant_pizzas():
-    return ''
+    return make_response(jsonify(pizza_list),200)
+
+@app.route('/restaurant_pizzas', methods = ["GET","POST"])
+def restaurant_pizzas():
+    if request.method == "POST":
+        new_pizza = RestaurantPizza(
+            price = request.json["price"],
+            pizza_id = request.json["pizza_id"],
+            restaurant_id = request.json["restaurant_id"]
+        )
+        validation_errors = []
+        if new_pizza.pizza_id not in [pizza.id for pizza in Pizza.query.all()]:
+            validation_errors.append("Pizza does not exist")
+        if new_pizza.restaurant_id not in [restaurant.id for restaurant in Restaurant.query.all()]:
+            validation_errors.append("Restaurant does not exist")
+        if len(validation_errors) == 0:
+            db.session.add(new_pizza)
+            db.session.commit()
+            return make_response(jsonify(new_pizza.to_dict_summary()),200)
+        else:
+            return make_response(jsonify({"errors": validation_errors}),403)
+            
+
+
+        # pizza = Pizza.query.filter(Pizza.id == new_pizza.pizza_id).first()
+        # pizza_dict = pizza.to_dict()
+
 
 
 if __name__ == '__main__':
--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -1,18 +1,95 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DATETIME, ForeignKey,CheckConstraint
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
+
+class PizzaError(Exception):
+    pass
+class RestaurantError(Exception):
+    pass
 
 class Restaurant(db.Model):
     __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    def to_dict_summary(self):
+        rest_dict = {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address
+        }
+        return rest_dict
+    
+    def to_dict(self):
+        rest_dict = {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address,
+            "pizzas": [pizza.to_dict_summary() for pizza in RestaurantPizza.query.filter(RestaurantPizza.restaurant_id == self.id).all()]
+        }
+
+        return rest_dict
+
+
+    restaurants = db.relationship("RestaurantPizza", back_populates="restaurant")
 
 class Pizza(db.Model):
     __tablename__ = 'pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DATETIME)
+    updated_at = db.Column(db.DATETIME)
+
+    def to_dict(self):
+        pizza_dict = {
+            "id": self.id,
+            "name": self.name,
+            "ingredients": self.ingredients,
+        }   
+
+        return pizza_dict  
+    
+    pizzas = db.relationship("RestaurantPizza", back_populates="pizza")
 
 class RestaurantPizza(db.Model):
     __tablename__ = 'restaurant_pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    price = db.Column(db.Integer, db.CheckConstraint("price > 0 AND price < 31"))
+    created_at = db.Column(db.DATETIME)
+    updated_at = db.Column(db.DATETIME)
+    
+    def to_dict_summary(self):
+        pizza = Pizza.query.filter(Pizza.id == self.pizza_id).first()
+        pizza_dict = {
+            "id": self.id,
+            "name": pizza.name,
+            "ingredients": pizza.ingredients,
+        }
+
+        return pizza_dict
+    
+    # @validates("pizza_id")
+    # def validate_pizza(self,key,pizza_id):
+    #     if pizza_id not in [pizza.id for pizza in Pizza.query.all()]:
+    #         raise PizzaError("Pizza does not exist")
+    #     return pizza_id
+    
+    # @validates("restaurant_id")
+    # def validate_restaurant(self,key,restaurant_id):
+    #     if restaurant_id not in [restaurant.id for restaurant in Restaurant.query.all()]:
+    #         raise RestaurantError("Restaurant does not exist")
+    #     return restaurant_id
+
+
+
+    pizza = db.relationship("Pizza", back_populates="pizzas")
+    restaurant = db.relationship("Restaurant", back_populates="restaurants")
